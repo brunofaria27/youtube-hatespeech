c3b91399a8ffaee6009be7d50f9f0528e1e5b572
Bruno Faria <bruno.fariadevl@gmail.com>
2024-04-24 00:15:36 -0300

    set apikey constructor

--- youtube-extract/src/main/java/youtube/analytics/util/YouTubeDataFetcher.java
+++ youtube-extract/src/main/java/youtube/analytics/util/YouTubeDataFetcher.java
@@ -1,5 +1,6 @@
 package youtube.analytics.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import youtube.analytics.model.MainComment;
 import youtube.analytics.model.ReplyComment;
 
@@ -7,14 +8,15 @@ import java.io.IOException;
 import java.util.List;
 
 public class YouTubeDataFetcher {
-    private String API_KEY;
+    private static String API_KEY;
+
+    public YouTubeDataFetcher() {
+        Dotenv dotenv = Dotenv.configure().load();
+        API_KEY = dotenv.get("API_KEY");
+    }
 
     public String getAPI_KEY() {
         return API_KEY;
-    }
-
-    public void setAPI_KEY(String API_KEY_GOOGLE) {
-        API_KEY = API_KEY_GOOGLE;
     }
     
     public static List<MainComment> fetchVideoComments(String videoId) throws IOException {
